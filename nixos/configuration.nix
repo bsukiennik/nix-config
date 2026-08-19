9d522cdc21388913ca1ccef4d896791ae0535054
bsukiennik <bastien.sukiennik@epitech.eu>
2026-08-19 22:47:44 +0200

    fix: bah nsm ca marche pas gdm

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -87,7 +87,7 @@
   services.xserver.enable = true;
 
   # Enable display manager  
-  services.displayManager.gdm.enable = true;
+  services.displayManager.sddm.enable = true;
 
   # Enable window manager
   programs.niri.enable = true;
