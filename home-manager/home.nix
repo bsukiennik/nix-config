71352392f41cc1b48d99f5093b3ee07544ec5c41
bsukiennik <bastien.sukiennik@epitech.eu>
2026-08-21 19:20:10 +0200

    add: xdg portal to make discord and x11 app work

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -68,6 +68,17 @@
     enable = true;
   };
 
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gnome
+      xdg-desktop-portal-gtk
+    ];
+    config.niri = {
+      default = [ "gnome" "gtk" ];
+    };
+  };
+
   xdg.configFile."niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink "/home/bassme/code/nix-config/dotfiles/niri/config.kdl";
 
   programs.noctalia = {
