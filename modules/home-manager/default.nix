d812b3818c8c71a902305a394cf23c4e62d9530f
bsukiennik <bastien.sukiennik@epitech.eu>
2026-08-22 18:24:43 +0200

    fix: mv fish.nix module to a fish folder

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -2,7 +2,7 @@
 # These should be stuff you would like to share with others, not your personal configurations.
 {
   # List your module files here
-  fish = import ./fish.nix;
+  fish = import ./fish/fish.nix;
   noctalia = import ./noctalia/noctalia.nix;
   niri = import ./niri/niri.nix;
 }
